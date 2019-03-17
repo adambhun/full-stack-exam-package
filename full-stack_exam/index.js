ac5a367f3e6c0581122459d95d4473250295f0cc
Adam Pisky-Nagy <adam_nagy89@yahoo.com>
2019-03-17 02:10:18 +0100

    Added server initialization and parser init using express. Database initialization, connection and error handling using mysql.

---
+++ full-stack_exam/index.js
@@ -0,0 +1,54 @@
+'use strict';
+
+/* Module dependencies */
+
+var express = require('express');
+var path = require('path');
+var mysql = require('mysql');
+exports = module.exports;
+
+/* Exposing konzola */
+
+exports.konzola = function (input) {
+    console.log(input);
+};
+
+/* Exposing server initialization */
+
+exports.serverInit = function (portNumber, staticFolder, expressApp, express) {
+    expressApp.use("/" + staticFolder, express.static("" + staticFolder));
+    expressApp.use(express.json());
+    expressApp.use(express.urlencoded({ extended: true }));
+    expressApp.listen(portNumber, function () {
+        var welcomeMessages = ["What is thy bidding, my master?", "Haile Selassie!", "And The Lord said, let there be light\n    and there was light.", "Do or do not! There is no try.", "Blahblahblahblahblah", "Hello, Boss!", "3,14"];
+        var randomMessage = Math.floor(Math.random() * 10);
+        console.log("Server at localhost:" + portNumber);
+        console.log("Initialized JSON and urlencoded parsers, static folder set to '/" + staticFolder + "'");
+        console.log(welcomeMessages[randomMessage]);
+    });
+};
+
+/* Exposing MySQL DB initializer, connector and error handler */
+
+exports.dbInit = function () {
+    mysql.createConnection({
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PW,
+        database: process.env.DB_NAME
+    });
+    console.log("Dont't you forget the .env file...(DB_HOST, DB_USER, DB_PW, DB_NAME)");
+};
+exports.dbConnect = function (databaseName) {
+    databaseName.connect(function (error) {
+        error ? console.log("Something prevents connection to " + databaseName + " DB :'(") :
+            console.log("Connected to " + databaseName + " DB with great success! Keep it up, Boss!");
+    });
+};
+exports.dbError = function (response, error) {
+    error.search('1062') !== -1 ?
+        response.send("The ID is already taken, please provide another one!") :
+        error ? (console.log(error),
+            response.status(500).send("The data query was unsuccessful due to internal server connection error. :'(")) :
+            null;
+};

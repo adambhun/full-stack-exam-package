0d265f655023d22928e67311964eed3307fea32b
Adam Pisky-Nagy <adam_nagy89@yahoo.com>
2019-03-18 21:48:30 +0100

    Added mysql class and constructor. Next I'll try to add a property which stores the last rows data retrieved and split the insertandreceive method into two. Also want to get rid of res.send/json etc inside these and store the retrieved data in a property.

--- full-stack_exam/index.js
+++ full-stack_exam/index.js
@@ -1,5 +1,4 @@
 'use strict';
-var _this = this;
 /* Module dependencies */
 require('dotenv').config();
 var express = require('express');
@@ -24,51 +23,65 @@ exports.serverInit = function (portNumber, staticFolder, expressApp, express) {
     });
 };
 /* Exposing MySQL DB initializer, connector and error handler */
-exports.testDB = function (dbName) {
-    _this.db = mysql.createConnection({
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PW,
-        database: process.env.DB_NAME
-    });
-    _this.connect = _this.db.connect(function (error) {
-        error ? console.log("Something prevents connection to " + dbName + " DB :'(") :
-            console.log("Connected to " + dbName + " DB with great success! Keep it up, Boss!");
-    });
-    _this.errorHandling = function (error, response) {
+var MySQL = /** @class */ (function () {
+    function MySQL(dbName) {
+        this.dbName = dbName;
+        this.okPacket = undefined;
+        this.uniqueID = undefined;
+        this.db = mysql.createConnection({
+            host: process.env.DB_HOST,
+            user: process.env.DB_USER,
+            password: process.env.DB_PW,
+            database: process.env.DB_NAME
+        });
+    }
+    MySQL.prototype.connect = function () {
+        var _this = this;
+        this.db.connect(function (error) {
+            error ? console.log("Something prevents connection to " + _this.dbName + " DB :'(") :
+                console.log("Connected to " + _this.dbName + " DB with great success! Keep it up, Boss!");
+        });
+    };
+    MySQL.prototype.errorHandling = function (error, response) {
         error && error.errno === 1062 ?
-            response.send("The ID is already taken, please provide another one!") :
+            response.status(400).send("The ID is already taken, please provide another one!") :
             error ?
                 (console.log(error),
                     response.status(500).send("The data query was unsuccessful due to internal server connection error. :'(")) :
                 null;
     };
-    _this.select = function (response, table, column) {
-        _this.db.query("SELECT " + column.join(',') + " FROM " + table, function (error, rows) {
-            _this.db.errorHandling(error, response);
-            response.json(rows);
+    ;
+    MySQL.prototype.select = function (response, table, column) {
+        var _this = this;
+        this.db.query("SELECT " + column.join(',') + " FROM " + table, function (error, rows) {
+            _this.errorHandling(error, response);
+            response.status(200).json(rows);
         });
     };
-    _this.okPacket = undefined;
-    _this.uniqueID = undefined;
-    _this.insertAndReceive = function (response, targetTable, insertToColumns, selectColumns) {
+    ;
+    MySQL.prototype.insertAndReceive = function (response, targetTable, insertToColumns, selectColumns) {
+        var _this = this;
         var valuesToInsert = [];
         for (var _i = 4; _i < arguments.length; _i++) {
             valuesToInsert[_i - 4] = arguments[_i];
         }
         valuesToInsert = valuesToInsert.map(function (value) { return value = "\"" + value + "\""; });
-        _this.db.query("INSERT INTO " + targetTable + " (" + insertToColumns.join(",") + ") VALUES (" + valuesToInsert.join(',') + ");", function (error, okPacket) {
+        this.db.query("INSERT INTO " + targetTable + " (" + insertToColumns.join(",") + ") VALUES (" + valuesToInsert.join(',') + ");", function (error, okPacket) {
             _this.okPacket = okPacket;
             _this.errorHandling(error, response);
             _this.okPacket !== undefined ?
-                _this.db.query("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA = \"" + dbName + "\" AND TABLE_NAME = \"" + targetTable + "\" AND EXTRA = \"auto_increment\";", function (error, rows) {
+                _this.db.query("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA = \"" + _this.dbName + "\" AND TABLE_NAME = \"" + targetTable + "\" AND EXTRA = \"auto_increment\";", function (error, rows) {
                     _this.errorHandling(error, response);
                     _this.uniqueID = rows[0].COLUMN_NAME;
                     _this.db.query("SELECT " + selectColumns.join(',') + " FROM " + targetTable + " WHERE " + _this.uniqueID + " = \"" + _this.okPacket.insertId + "\";", function (error, rows) {
                         _this.errorHandling(error, response);
-                        response.json(rows);
+                        response.status(200).json(rows);
                     });
                 }) : null;
         });
     };
-};
+    ;
+    return MySQL;
+}());
+;
+exports.mysqlPlus = MySQL;

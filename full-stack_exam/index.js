eb04c007bb734a2a23b39545579c25b32b459175
Adam Pisky-Nagy <adam_nagy89@yahoo.com>
2019-03-20 10:50:38 +0100

    Saving before making the callback of the query async, trying to wait for the result, which is okPacket.insertId.

--- full-stack_exam/index.js
+++ full-stack_exam/index.js
@@ -1,4 +1,39 @@
 'use strict';
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
 /* Module dependencies */
 require('dotenv').config();
 var express = require('express');
@@ -51,8 +86,9 @@ exports.HeadSetter = HeaderSettings;
 var MySqlPlus = /** @class */ (function () {
     function MySqlPlus(dbName) {
         this.dbName = dbName;
-        this.okPacket = undefined;
+        this.insertId = undefined;
         this.uniqueID = undefined;
+        this.rowsData = undefined;
         this.db = mysql.createConnection({
             host: process.env.DB_HOST,
             user: process.env.DB_USER,
@@ -79,34 +115,58 @@ var MySqlPlus = /** @class */ (function () {
                         error && error.errno === 1054 ?
                             response.status(400).send("Syntax error detected at VALUES!") :
                             error ?
-                                (console.log(error),
+                                (console.error(error),
                                     response.status(500).send("The data query was unsuccessful due to internal server connection error. :'(")) :
                                 null;
     };
     ;
-    MySqlPlus.prototype.select = function (response, table, column) {
+    MySqlPlus.prototype.findUniqueID = function (response, targetTable) {
         var _this = this;
-        this.db.query("SELECT " + column.join(',') + " FROM " + table, function (error, rows) {
+        this.db.query("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA = \"" + this.dbName + "\" \n      AND TABLE_NAME = \"" + targetTable + "\" AND EXTRA = \"auto_increment\";", function (error, rows) {
+            _this.errorHandling(error, response);
+            _this.uniqueID = rows[0].COLUMN_NAME;
+        });
+    };
+    ;
+    MySqlPlus.prototype.select = function (response, targetColumns, targetTable) {
+        var _this = this;
+        this.db.query("SELECT " + targetColumns.join(',') + " FROM " + targetTable, function (error, rows) {
             _this.errorHandling(error, response);
             response.status(200).json(rows);
         });
     };
     ;
-    MySqlPlus.prototype.insert = function (response, targetTable, insertToColumns, valuesToInsert, selectColumns) {
+    MySqlPlus.prototype.selectFilter = function (response, targetTable, targetColumns, columnToFilter, valueToFilter) {
         var _this = this;
-        valuesToInsert = valuesToInsert.map(function (value) { return value = "\"" + value + "\""; });
-        this.db.query("INSERT INTO " + targetTable + " (" + insertToColumns.join(",") + ") \n      VALUES (" + valuesToInsert.join(',') + ");", function (error, okPacket) {
-            _this.okPacket = okPacket;
+        this.db.query("SELECT " + targetColumns.join(',') + " FROM " + targetTable + "\n      WHERE " + columnToFilter + " = " + valueToFilter + ";", function (error, rows) {
             _this.errorHandling(error, response);
-            _this.okPacket !== undefined ?
-                _this.db.query("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA = \"" + _this.dbName + "\" \n      AND TABLE_NAME = \"" + targetTable + "\" AND EXTRA = \"auto_increment\";", function (error, rows) {
-                    _this.errorHandling(error, response);
-                    _this.uniqueID = rows[0].COLUMN_NAME;
-                    _this.db.query("SELECT " + selectColumns.join(',') + " FROM " + targetTable + " \n      WHERE " + _this.uniqueID + " = \"" + _this.okPacket.insertId + "\";", function (error, rows) {
-                        _this.errorHandling(error, response);
-                        response.status(200).json(rows);
-                    });
-                }) : null;
+            response.status(200).json(rows);
+        });
+    };
+    ;
+    MySqlPlus.prototype.insert = function (response, targetTable, insertToColumns, valuesToInsert) {
+        return __awaiter(this, void 0, void 0, function () {
+            var getId;
+            var _this = this;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0:
+                        valuesToInsert = valuesToInsert.map(function (value) { return value = "\"" + value + "\""; });
+                        getId = new Promise(function (resolve, reject) {
+                            _this.db.query("INSERT INTO " + targetTable + " (" + insertToColumns.join(",") + ") VALUES (" + valuesToInsert.join(',') + ");", function (error, okPacket) {
+                                error ?
+                                    _this.errorHandling(error, response) :
+                                    _this.db.insertId = okPacket.insertId;
+                                /* this.findUniqueID(response, targetTable); */
+                                resolve(_this.db.insertId);
+                            });
+                        });
+                        return [4 /*yield*/, getId];
+                    case 1:
+                        _a.sent();
+                        return [2 /*return*/];
+                }
+            });
         });
     };
     ;
@@ -114,57 +174,15 @@ var MySqlPlus = /** @class */ (function () {
         var _this = this;
         this.db.query("DELETE FROM " + targetTable + " WHERE " + columnToFilter + " = \"" + statementToFilter + "\";", function (error, okPacket) {
             _this.errorHandling(error, response);
-            response.status(200).send("Data deleted successfully!");
+            response.end();
         });
     };
     ;
-    MySqlPlus.prototype.update = function (response, targetTable, columnToUpdate, newValueOrExpression, columnToFilter, statementToFilter) {
+    MySqlPlus.prototype.update = function (response, targetTable, columnToUpdate, newValueOrExpression, columnToFilter, valueToFilter) {
         var _this = this;
-        var columnsToReceive = [];
-        for (var _i = 6; _i < arguments.length; _i++) {
-            columnsToReceive[_i - 6] = arguments[_i];
-        }
-        this.db.query("UPDATE " + targetTable + " SET " + columnToUpdate + " = \"" + newValueOrExpression + "\"\n      WHERE " + columnToFilter + " = " + statementToFilter + ";", function (error, okPacket) {
+        this.db.query("UPDATE " + targetTable + " SET " + columnToUpdate + " = \"" + newValueOrExpression + "\"\n      WHERE " + columnToFilter + " = " + valueToFilter + ";", function (error, okPacket) {
             _this.errorHandling(error, response);
-            _this.db.query("SELECT " + columnsToReceive.join(',') + " FROM " + targetTable + " \n      WHERE " + columnToUpdate + " = \"" + newValueOrExpression + "\";", function (error, rows) {
-                _this.errorHandling(error, response);
-                response.status(200).json(rows);
-            });
-        });
-    };
-    ;
-    MySqlPlus.prototype.insertAndUpdate = function (response, targetTable, insertToColumns, valuesToInsert, columnToUpdate, newValueOrExpression, columnToFilter, statementToFilter, columnsToReceive) {
-        var _this = this;
-        valuesToInsert = valuesToInsert.map(function (value) { return value = "\"" + value + "\""; });
-        this.db.query("INSERT INTO " + targetTable + " (" + insertToColumns.join(",") + ") \n      VALUES (" + valuesToInsert.join(',') + ");", function (error, okPacket) {
-            _this.okPacket = okPacket;
-            _this.errorHandling(error, response);
-            _this.okPacket !== undefined ?
-                _this.db.query("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA = \"" + _this.dbName + "\" \n      AND TABLE_NAME = \"" + targetTable + "\" AND EXTRA = \"auto_increment\";", function (error, rows) {
-                    _this.errorHandling(error, response);
-                    _this.uniqueID = rows[0].COLUMN_NAME;
-                    _this.db.query("UPDATE " + targetTable + " SET " + columnToUpdate + " = \"" + newValueOrExpression + "\"\n      WHERE " + columnToFilter + " = " + statementToFilter + ";", function (error, okPacket) {
-                        _this.errorHandling(error, response);
-                        _this.db.query("SELECT " + columnsToReceive.join(',') + " FROM " + targetTable + " \n      WHERE " + _this.uniqueID + " = \"" + _this.okPacket.insertId + "\";", function (error, rows) {
-                            _this.errorHandling(error, response);
-                            response.status(200).json(rows);
-                        });
-                    });
-                }) : null;
-        });
-    };
-    ;
-    MySqlPlus.prototype.deleteAndUpdate = function (response, targetTable, columnToFilter, statementToFilter, columnToUpdate, newValueOrExpression, columnToFilterOnUpdate, statementToFilterOnUpdate, columnsToReceive) {
-        var _this = this;
-        this.db.query("DELETE FROM " + targetTable + " WHERE " + columnToFilter + " = \"" + statementToFilter + "\";", function (error, okPacket) {
-            _this.errorHandling(error, response);
-            _this.db.query("UPDATE " + targetTable + " SET " + columnToUpdate + " = \"" + newValueOrExpression + "\"\n      WHERE " + columnToFilterOnUpdate + " = " + statementToFilterOnUpdate + ";", function (error, okPacket) {
-                _this.errorHandling(error, response);
-                _this.db.query("SELECT " + columnsToReceive.join(',') + " FROM " + targetTable + " \n      WHERE " + columnToFilterOnUpdate + " = \"" + statementToFilterOnUpdate + "\";", function (error, rows) {
-                    _this.errorHandling(error, response);
-                    response.status(200).json(rows);
-                });
-            });
+            response.end();
         });
     };
     ;
